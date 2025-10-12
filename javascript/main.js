35b812e9bbbc5666759c0417c06b70971d2354a3
Bruno Daniel <bdao.profissional@gmail.com>
2025-10-11 23:09:21 -0300

    crie repeticoes (Lacos)

--- javascript/main.js
+++ javascript/main.js
@@ -32,6 +32,23 @@ console.log( listaDeArrays );    // [ 'html', 'css', 'javascript', 'PowerShell' 
 console.log( listaDeArrays.splice( 2, 2 ) );    // [ 'javascript', 'PowerShell' ]
 console.log( listaDeArrays );    // [ 'html', 'css' ]
 
+let ninja = [ 'Naruto', 'Sasuke', 'Sakura', 'kakashi' ];
+for ( let i = 0; i < ninja.length; i ++ ) {
+    console.log( 'Eu sou: ' + ninja[ i ] );    // Eu sou: Naruto    Eu sou: Sasuke    Eu sou: Sakura    Eu sou: kakashi
+}
+
+let aldeia = 0;
+while ( aldeia < ninja.length ) {
+    console.log( 'Eu sou: ' + ninja[ aldeia ] + ' um(a) ninja da aldeia da folha' );    // Eu sou: Naruto um(a) ninja da aldeia da folha    Eu sou: Sasuke um(a) ninja da aldeia da folha    Eu sou: Sakura um(a) ninja da aldeia da folha    Eu sou: kakashi um(a) ninja da aldeia da folha
+    aldeia ++;
+}
+
+let timeSete = 0;
+do {
+    console.log( 'Eu sou: ' + ninja[ timeSete ] + ' do time 7' );    // Eu sou: Naruto do time 7    Eu sou: Sasuke do time 7    Eu sou: Sakura do time 7    Eu sou: kakashi do time 7
+    timeSete ++;
+} while ( timeSete < ninja.length );
+
 
 
 
@@ -49,3 +66,6 @@ console.log( listaDeArrays );    // [ 'html', 'css' ]
 // .length     Retorna o número de elementos de um array ou o número de caracteres de uma string
 // push()     Adiciona um ou mais elementos ao final de um array
 // splice()     Remove ou substitui elementos em qualquer posição de um array
+// for (){}     Estrutura de controle de fluxo usada para repetir um bloco de código várias vezes
+// while (){}     Estrutura de controle de fluxo usada para repetir um bloco de código, mas de forma condicional
+// do {} while ()     Variação do laço while, a diferença é que o bloco de código é executado pelo menos uma vez
