a8d7ea7e1664c5b76d318752e35c23a4a90a8e4e
Bruno Daniel <bdao.profissional@gmail.com>
2025-10-11 22:54:31 -0300

    crie variaveis e constantes

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+let variavelMutavel = 'Valor que pode ser substituído.';
+const variavelImutavel = 'Valor que não pode ser substituído e deve ser iniciado com um valor.';
+console.log( variavelMutavel );    // Valor que pode ser substituído
+console.log( variavelImutavel );    // Valor que não pode ser substituído e deve ser iniciado com um valor.
+variavelMutavel = 'Valor mudado.';
+console.log( variavelMutavel );    // Valor mudado.
+
+
+
+
+// =     Atribui um valor a uma variável
+// ==     Compara apenas o valor, podendo converter o tipo
+// ===     Compara o valor e o tipo. Não faz conversão
+// ''     O tipo mais comum para criar strings simples
+// ""     Também muito comum e funciona da mesma forma que as aspas simples
+// console     Objeto global que fornece acesso à interface de depuração (debug) do ambiente de execução
+// log()     Método do objeto console, exibe informações na tela do console
