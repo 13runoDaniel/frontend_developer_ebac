faf6c7c2ed2d21948ce697311e214075379f0618
Bruno Daniel <bdao.profissional@gmail.com>
2025-10-11 23:03:56 -0300

    explore os tipos de dados

--- javascript/main.js
+++ javascript/main.js
@@ -5,6 +5,22 @@ console.log( variavelImutavel );    // Valor que não pode ser substituído e de
 variavelMutavel = 'Valor mudado.';
 console.log( variavelMutavel );    // Valor mudado.
 
+let string = '5';
+console.log( typeof( string ) );    // string
+let number = 5;
+console.log( typeof( number ) );    // number
+let indefinida;
+console.log( typeof( indefinida ) );    // undefined
+let booleanoVerdadeiro = true;
+console.log( typeof( booleanoVerdadeiro ) );    // boolean
+let booleanoFalso = false;
+console.log( typeof( booleanoFalso ) );    // boolean
+number = '5';
+console.log( typeof( number ) );    // string
+number = parseInt( number );
+console.log( number );    // 5
+console.log( typeof( parseInt( 8 ) ) );    // number
+
 
 
 
@@ -15,3 +31,6 @@ console.log( variavelMutavel );    // Valor mudado.
 // ""     Também muito comum e funciona da mesma forma que as aspas simples
 // console     Objeto global que fornece acesso à interface de depuração (debug) do ambiente de execução
 // log()     Método do objeto console, exibe informações na tela do console
+// NaN     Valor especial que significa não é um número
+// typeof()     Verifica o tipo de dados de uma variável
+// parseInt()     Analisa uma string e retorna um número inteiro
