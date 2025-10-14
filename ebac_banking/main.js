a79b9f6970584786564cd1c9c32e7902fa67c696
Bruno Daniel <bdao.profissional@gmail.com>
2025-10-13 22:03:15 -0300

    estilize elementos com JavaScript

--- ebac_banking/main.js
+++ ebac_banking/main.js
@@ -1,4 +1,6 @@
 const form = document.getElementById( 'form-deposito' );
+const nomeDoBeneficiario = document.getElementById( 'nome-beneficiario' );
+let formEstaValido = false;
 
 function validarNome( nomeCompleto ) {
     const nomeComoArray = nomeCompleto.split( ' ' );
@@ -6,27 +8,38 @@ function validarNome( nomeCompleto ) {
 }
 
 form.addEventListener( 'submit', function ( event ) {
-    let formEstaValido = false;
     event.preventDefault();
 
-    const nomeDoBeneficiario = document.getElementById( 'nome-beneficiario' );
-    const numeroDaContaDoBeneficiario = document.getElementById( 'numero-conta' );
     const valorDoDeposito = document.getElementById( 'valor-deposito' );
-    const mensagemDeSucesso = `Montante de R$ ${ valorDoDeposito.value } depositado para o cliente: ${ nomeDoBeneficiario.value }, na conta ${ numeroDaContaDoBeneficiario.value }`;
+    const numeroDaContaDoBeneficiario = document.getElementById( 'numero-conta' );
+    const mensagemDeSucesso = `Montante de R$ <b>${ valorDoDeposito.value }</b> depositado para o cliente: <b>${ nomeDoBeneficiario.value }</b>, na conta <b>${ numeroDaContaDoBeneficiario.value }</b>`;
 
     formEstaValido = validarNome( nomeDoBeneficiario.value );
     if ( formEstaValido ) {
-        alert( mensagemDeSucesso );
+        const containerMensagemDeSucesso = document.querySelector( '.success-message' ); 
+        containerMensagemDeSucesso.innerHTML = mensagemDeSucesso;
+        containerMensagemDeSucesso.style.display = 'block';
 
         nomeDoBeneficiario.value = '';
         numeroDaContaDoBeneficiario.value = '';
         valorDoDeposito.value = '';
     } else {
-        alert( 'O nome não está completo' );
+        nomeDoBeneficiario.style.border = '1px solid red';
+        document.querySelector( '.error-message' ).style.display = 'block'; 
     }
 } );
 
-console.log( form );
+nomeDoBeneficiario.addEventListener( 'keyup', function ( e ) {
+    formEstaValido = validarNome( e.target.value );
+
+    if ( !formEstaValido ) {
+        nomeDoBeneficiario.classList.add( 'error' );
+        document.querySelector( '.error-message' ).style.display = 'block'; 
+    } else {
+        nomeDoBeneficiario.classList.remove( 'error' );
+        document.querySelector( '.error-message' ).style.display = 'none'; 
+    }
+} );
 
 
 
@@ -41,3 +54,7 @@ console.log( form );
 // alert()     Exibe uma caixa de diálogo pop-up simples no navegador com uma mensagem
 // ``     A forma mais moderna e flexível que permite interpolar variáveis usando ${ minhaVariavel } e usar quebras de linha
 // ${  }     Permite embutir valores dinâmicos ou resultados de código JavaScript dentro de uma string
+// .innerHTML     Obtém ou define o conteúdo HTML de um elemento específico do DOM
+// 'change'     Detecta quando o valor de um elemento de formulário é modificado e o elemento perde o foco
+// 'keyup'     Detecta quando uma tecla é solta após ter sido pressionada
+// .classList .add() (adicionar), .remove()(remover)     Alterna ou verifica se um elemento possui uma classe específica
